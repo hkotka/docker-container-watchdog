3fa77976b612458edbb2033ee863c902bdba7942
Henri Kotka <henri.kotka@gmail.com>
2019-08-04 13:19:53 +0300

    Seperated code into clearer functions

--- container-watchdog.py
+++ container-watchdog.py
@@ -24,7 +24,6 @@ except Exception as e:
     logging.fatal("%s", e)
     exit()
 
-# Function for sending Slack webhook messages
 def sendSlackMessage(slack_message_content):
     if slack_webhook_url != "":
         try:
@@ -33,6 +32,35 @@ def sendSlackMessage(slack_message_content):
         except Exception as e:
             logging.error("%s", e)
 
+# Check and return container health status. If 'Health' key doesn't exist for container(healthcheck not set), log exception.
+def getContainerHealthStatus(container):
+    try:
+        containerHealthStatus = container.attrs['State']['Health']['Status']
+    except KeyError:
+        containerHealthStatus = 'nokey'
+    return containerHealthStatus
+
+def restartContainer(container):
+    try:
+        container.restart()
+        logging.info("Restarted container: %s",container.name)
+        slack_message_content['text'] = ("[Container watchdog]: has *_restarted_* container: [ *_{0}_* ] which had healthstatus: [ _{1}_ ] and state:"
+                                        " [ _{2}_ ] on hostmachine [ _{3}_ ]".format(container.name, containerHealthStatus,
+                                         containerStatus, dockerHost))
+# Add container id to a list of restarted containers, this will be used to check weather container has recovered after restart
+        if container.short_id not in restartedContainers:
+            restartedContainers.append(container.short_id)
+    except Exception as e:
+        logging.fatal("%s", e)
+        slack_message_content['text'] = ("[Container watchdog]: Docker daemon failed to restart container *{0}* on hostmachine *{1}*"
+                                        " with error message: _{2}_".format(container.name, dockerHost, e))
+
+def containerRecovered(container):
+    logging.info("Container %s has recovered and is now healthy!", container.name)
+    slack_message_content['text'] = ("[Container watchdog]: Container: [ *_{0}_* ] has *recovered* with healthstatus: [ _{1}_ ] and state: [ _{2}_ ]"
+                                    " on hostmachine [ _{3}_ ]".format(container.name, containerHealthStatus, containerStatus, dockerHost))
+    restartedContainers.remove(container.short_id)
+
 
 # Run loop indefinetly polling every 30 seconds normally or in 15 minutes after watchdog has restarted a container.
 while True:
@@ -40,38 +68,20 @@ while True:
     ContainerList = client.containers.list(all)
     for container in ContainerList:
         containerStatus = container.status
-# Check if Healthstatus is set for the container and If 'Health' key doesn't exist for container(healthcheck not set), log exception.
-        try:
-            healthStatus = container.attrs['State']['Health']['Status']
-        except KeyError:
-            logging.debug("Healthcheck has not been defined for container %s", container.name)
-            healthStatus = 'nokey'
+        containerHealthStatus = getContainerHealthStatus(container)
 # Check if the container was restarted previously and is now healthy, log and send Slack message if recovered and remove from a list of restarted containers
-        if container.short_id in restartedContainers and healthStatus == 'healthy':
-            logging.info("Container %s has recovered and is now healthy!", container.name)
-            slack_message_content['text'] = ("[Container watchdog]: Container: [ *_{0}_* ] has *recovered* with healthstatus: [ _{1}_ ] and state: [ _{2}_ ]"
-                                            " on hostmachine [ _{3}_ ]".format(container.name, healthStatus, containerStatus, dockerHost))
+        if container.short_id in restartedContainers and containerHealthStatus == 'healthy':
+            containerRecovered(container)
             sendSlackMessage(slack_message_content)
-            restartedContainers.remove(container.short_id)
 #  If container is in unhealthy or exited status, restart. Send Slack message when trying to restart container. Add container to list of restarted containers.
-        elif healthStatus == 'unhealthy' or containerStatus == 'exited':
+        elif containerHealthStatus == 'unhealthy' or containerStatus == 'exited':
             logging.error("Found container in unhealthy state! Container: %s has health status: %s and container status: %s",
-                         container.name, healthStatus, containerStatus)
-            try:
-                container.restart()
-                logging.info("Restarted container: %s",container.name)
-                slack_message_content['text'] = ("[Container watchdog]: has *_restarted_* container: [ *_{0}_* ] which had healthstatus: [ _{1}_ ] and state:"
-                                                " [ _{2}_ ] on hostmachine [ _{3}_ ]".format(container.name, healthStatus, containerStatus, dockerHost))
-                sendSlackMessage(slack_message_content)
-                if container.short_id not in restartedContainers:
-                    restartedContainers.append(container.short_id)
-            except Exception as e:
-                logging.fatal("%s", e)
-                slack_message_content['text'] = ("[Container watchdog]: Docker daemon failed to restart container *{0}* on hostmachine *{1}*"
-                                                " with error message: _{2}_".format(container.name, dockerHost, e))
-                sendSlackMessage(slack_message_content)
+                         container.name, containerHealthStatus, containerStatus)
+            restartContainer(container)
+            sendSlackMessage(slack_message_content)
             restartStatus = True
-        logging.debug('%s - %s - %s', container.name, healthStatus, containerStatus)
+        logging.debug('%s - %s - %s', container.name, containerHealthStatus, containerStatus)
+
 # Wait to poll again, longer if restarts were done in previous loop
     if restartStatus is True:
         logging.info("Waiting %s seconds until next polling, because container was restarted", timerRestarted)
